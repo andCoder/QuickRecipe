c218ed0f0cbd072aa6fd5616f6c77b20aeb6c902
andCoder <andrey1247@mail.ru>
2018-12-08 21:41:10 +1000

    Added property attributes.

--- QuickRecipe.Data/Context/AppDbContext.cs
+++ QuickRecipe.Data/Context/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
+using QuickRecipe.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +13,17 @@ namespace QuickRecipe.Data.Context
         {
         }
 
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Step> Steps { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Recipe>()
+                .HasMany(r => r.Steps)
+                .WithOne(s => s.Parent)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
