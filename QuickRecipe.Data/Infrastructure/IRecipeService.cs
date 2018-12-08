3d7c2ecf07f21cb3d5e71a6b8e1090b799339f99
andCoder <andrey1247@mail.ru>
2018-12-08 21:20:29 +1000

    Revert "Revert "Created basic models.""
    This reverts commit b64ace79ebe2bd595c043785694d3131aa96e378.

---
+++ QuickRecipe.Data/Infrastructure/IRecipeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickRecipe.Data.Infrastructure
+{
+    public interface IRecipeService
+    {
+        
+    }
+}
