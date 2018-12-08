f28237ce2b3737706d5674fd5f38812de6215895
andCoder <andrey1247@mail.ru>
2018-12-08 19:41:19 +1000

    Build database context.

---
+++ QuickRecipe.Data/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickRecipe.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
--- QuickRecipe/Startup.cs
+++ QuickRecipe/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using QuickRecipe.Data.Context;
 
 namespace QuickRecipe
 {
@@ -25,6 +27,10 @@ namespace QuickRecipe
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("AppConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connection));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
